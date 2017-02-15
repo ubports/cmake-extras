f72795155ecf4d67261168cab7dddf618435b98b
Rodney Dawes <rodney.dawes@canonical.com>
2017-02-15 13:43:05 -0500

    Refactor GSettings module to provide a compile_schemas macro.

--- src/GSettings/GSettingsConfig.cmake
+++ src/GSettings/GSettingsConfig.cmake
@@ -1,49 +1,80 @@
 # GSettingsConfig.cmake, CMake macros written for Marlin, feel free to re-use them.
+find_package(PkgConfig REQUIRED)
 
+# Find the binary for compiling schemas
+execute_process(
+  COMMAND ${PKG_CONFIG_EXECUTABLE} gio-2.0 --variable glib_compile_schemas
+  OUTPUT_VARIABLE _GLIB_COMPILE_SCHEMAS
+  OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+
+
+# Have an option to not install the schema into where GLib is
 option (GSETTINGS_LOCALINSTALL "Install GSettings schemas locally instead of to the GLib prefix" OFF)
-
-option (GSETTINGS_COMPILE "Compile GSettings schemas after installation" OFF)
-
-if(GSETTINGS_LOCALINSTALL)
+if (GSETTINGS_LOCALINSTALL)
     message(STATUS "GSettings schemas will be installed locally.")
-endif()
+    set (GSETTINGS_DIR "${CMAKE_INSTALL_PREFIX}/share/glib-2.0/schemas/")
+else (GSETTINGS_LOCALINSTALL)
+    execute_process (
+      COMMAND ${PKG_CONFIG_EXECUTABLE} glib-2.0 --variable prefix
+      OUTPUT_VARIABLE _glib_prefix
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    set (GSETTINGS_DIR "${_glib_prefix}/share/glib-2.0/schemas/")
+endif (GSETTINGS_LOCALINSTALL)
+message (STATUS "GSettings schemas will be installed into ${GSETTINGS_DIR}")
 
+# Have an option to compile the schemas once installed
+option (GSETTINGS_COMPILE "Compile GSettings schemas after installation" OFF)
 if(GSETTINGS_COMPILE)
     message(STATUS "GSettings schemas will be compiled.")
 endif()
 
 macro(add_schema SCHEMA_NAME)
-
-    set(PKG_CONFIG_EXECUTABLE pkg-config)
-    # Have an option to not install the schema into where GLib is
-    if (GSETTINGS_LOCALINSTALL)
-        SET (GSETTINGS_DIR "${CMAKE_INSTALL_PREFIX}/share/glib-2.0/schemas/")
-    else (GSETTINGS_LOCALINSTALL)
-        execute_process (COMMAND ${PKG_CONFIG_EXECUTABLE} glib-2.0 --variable prefix OUTPUT_VARIABLE _glib_prefix OUTPUT_STRIP_TRAILING_WHITESPACE)
-        SET (GSETTINGS_DIR "${_glib_prefix}/share/glib-2.0/schemas/")
-    endif (GSETTINGS_LOCALINSTALL)
-
     # Use the correct schema file as it may be generated
-    if (EXISTS ${CMAKE_CURRENT_BINARY_DIR}/${SCHEMA_NAME})
-      set (SCHEMA_FILE "${CMAKE_CURRENT_BINARY_DIR}/${SCHEMA_NAME}")
-    else ()
-      set (SCHEMA_FILE "${CMAKE_CURRENT_SOURCE_DIR}/${SCHEMA_NAME}")
+    if (NOT EXISTS ${CMAKE_CURRENT_BINARY_DIR}/${SCHEMA_NAME})
+      file (COPY ${SCHEMA_NAME} ${SCHEMA_NAME})
     endif ()
+    set (SCHEMA_FILE "${CMAKE_CURRENT_BINARY_DIR}/${SCHEMA_NAME}")
 
     # Run the validator and error if it fails
-    execute_process (COMMAND ${PKG_CONFIG_EXECUTABLE} gio-2.0 --variable glib_compile_schemas  OUTPUT_VARIABLE _glib_comple_schemas OUTPUT_STRIP_TRAILING_WHITESPACE)
-    execute_process (COMMAND ${_glib_comple_schemas} --dry-run --schema-file=${SCHEMA_FILE} ERROR_VARIABLE _schemas_invalid OUTPUT_STRIP_TRAILING_WHITESPACE)
+    execute_process (
+      COMMAND ${_GLIB_COMPILE_SCHEMAS} --dry-run --schema-file=${SCHEMA_FILE}
+      ERROR_VARIABLE _schemas_invalid
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
 
     if (_schemas_invalid)
       message (SEND_ERROR "Schema validation error: ${_schemas_invalid}")
     endif (_schemas_invalid)
 
-    # Actually install and recomple schemas
-    message (STATUS "GSettings schemas will be installed into ${GSETTINGS_DIR}")
+    # Install the schemas
     install (FILES ${SCHEMA_FILE} DESTINATION ${GSETTINGS_DIR} OPTIONAL)
 
-    if (GSETTINGS_COMPILE)
-        install (CODE "message (STATUS \"Compiling GSettings schemas\")")
-        install (CODE "execute_process (COMMAND ${_glib_comple_schemas} ${GSETTINGS_DIR})")
-    endif ()
+    # Add a rule to compile the schemas if so enabled
+    # FIXME: This should ideally only be called once, after all the files
+    # have been installed, but we must do so every time currently, due
+    # to a bug in cmake lacking ability to order last (LP: #1665006)
+    if(GSETTINGS_COMPILE)
+      install (CODE "compile_schemas (${GSETTINGS_DIR})")
+    endif()
+endmacro()
+
+macro(compile_schemas SCHEMA_DIR)
+  if (${SCHEMA_DIR} MATCHES "^${CMAKE_SOURCE_DIR}.*$")
+    set_property (
+      DIRECTORY "${CMAKE_SOURCE_DIR}"
+      APPEND
+      PROPERTY ADDITIONAL_MAKE_CLEAN_FILES "gschemas.compiled"
+    )
+  endif ()
+  execute_process (
+    COMMAND "${_GLIB_COMPILE_SCHEMAS}" "${SCHEMA_DIR}"
+    ERROR_VARIABLE _schema_compile_error
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
+  if (_schema_compile_error)
+    message (SEND_ERROR "Schemas compile failed: ${_schema_compile_error}")
+  endif (_schema_compile_error)
+  message (STATUS "Compiling GSettings schemas in: ${SCHEMA_DIR}")
 endmacro()
